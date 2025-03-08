37e500323a14cfe5b82ee23bbab42a63f8a5e98c
GrzegorzK121 <117106707+GrzegorzK121@users.noreply.github.com>
2025-03-08 20:49:05 +0100

    Create GK_BT_SCREEN.h

---
+++ components/GK_BT_SCREEN/include/GK_BT_SCREEN.h
@@ -0,0 +1,12 @@
+#ifndef GK_BT_SCREEN_H
+#define GK_BT_SCREEN_H
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_bt_defs.h"
+#include "esp_bt_main.h"
+#include "esp_gap_bt_api.h"
+
+void gk_bt_screen(void);
+
+#endif // GK_BT_SCREEN_H
